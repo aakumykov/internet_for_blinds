6aa04a0920a3cb9814e6da4e00152674d8e2168a
Андрей Кумыков <aakumykov@yandex.ru>
2019-05-02 00:07:24 +0700

    Результаты поиска видео с использованием внешних функций.

--- greasemonkey/video-mode/youtube-search-results-scroll.js
+++ greasemonkey/video-mode/youtube-search-results-scroll.js
@@ -4,6 +4,8 @@
 // @grant    none
 // @include  https://www.youtube.com/results?search_query=*
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
+// @require			 https://github.com/aakumykov/internet_for_blinds/raw/master/greasemonkey/lib/play_audio.js
+// @require			 https://github.com/aakumykov/internet_for_blinds/raw/master/greasemonkey/lib/html_functions.js
 // ==/UserScript==
 
 var $ = window.jQuery;
@@ -13,6 +15,9 @@ let scrollCallbackCounter = 0;
 
 // Запуск скрипта
 $(window).load(function() {
+
+  document.title = 'Результаты поиска видео';
+  //$(document).prop('title', 'Результаты поиска видео');
   
   $("html, body").animate({ scrollTop: $(document).height()*3 }, 5000, 'swing', function(){
   	let resSet = $("A#video-title");
@@ -37,11 +42,9 @@ function processPage() {
   let resSet = $("A#video-title");
    
   // Переделываю страницу
-  $(document.body).empty();
-  $(document.body).append("<style>BODY { padding: 20pt; } * { font-family: serif; } A { font-size: 2em; } H1 { text-align:center; padding-bottom: 20pt; }</style>");
-  $(document.body).append("<button id='focusStart'>Начальная позиция</button><br>");
-  $(document.body).append("<button>Результаты поиска видео по запросу &laquo;"+searchQuery+"&raquo;</button><br>");
+	clearPage("результатов поиска видео");
 
+  $(document.body).append("<style>BODY { padding: 20pt; } * { font-family: serif; } A { font-size: 2em; } H1 { text-align:center; padding-bottom: 20pt; }</style>");
   $(document.body).append("<ul id='resultsList'></ul>");
 
   resSet.each(function(index, element){
@@ -50,6 +53,11 @@ function processPage() {
     //$(document.body).append( $("<li><a href='"+href+"' target='_blank'>"+text+"</a></li>") );
     $("ul#resultsList").append( $("<li class='resultsItem'><a href='"+href+"' target='_blank'>"+text+"</a></li>") );
   });
+
   
-  $("#focusStart").focus();
+  playAudio('http://127.0.0.1/video_search_complete.mp3');
+  
+  document.title = "поиск видео: «" + searchQuery + "»";
 }
+
+
