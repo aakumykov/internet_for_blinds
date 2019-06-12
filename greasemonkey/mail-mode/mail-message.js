57c37c7c3a235befcf7e62c490ebb358ae164233
Андрей Кумыков <aakumykov@yandex.ru>
2019-06-12 20:09:39 +0700

    Увеличил размер <textarea> в сообщении.

--- greasemonkey/mail-mode/mail-message.js
+++ greasemonkey/mail-mode/mail-message.js
@@ -4,7 +4,9 @@
 // @grant    none
 // @include  https://mail.yandex.ru/lite/message/*
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
-// @require  https://raw.githubusercontent.com/aakumykov/static_test/master/date_functions.js
+// @require  https://github.com/aakumykov/internet_for_blinds/raw/master/greasemonkey/lib/date_functions.js
+// @require  https://raw.githubusercontent.com/aakumykov/internet_for_blinds/master/greasemonkey/lib/html_functions.js
+// @require  https://github.com/aakumykov/internet_for_blinds/raw/master/greasemonkey/lib/play-audio.js
 // ==/UserScript==
 
 var DEBUG = false;
@@ -34,17 +36,13 @@ function fetchDate(){
 function fetchAttachmentsCount(){
   let cnt = $('.b-message-attach__i').length;
   debugMsg("вложений: "+cnt);
-	return cnt;
+  return cnt;
 }
 
 function clearAttachments(){
   $('div.b-message-attach').each( function(index,element){
     element.remove();
   });
-}
-
-function clearPage() {
-  $('body').empty();
 }
 
 function buildLine(text){
@@ -76,28 +74,21 @@ function constructNewPage(msgSubject, msgBody, msgDate, attachmentsCount){
 }
 
 
-function playAudio(audioURL){
-  var audioTag = document.createElement('audio');
-  audioTag.src = audioURL;
-  audioTag.play();
-}
-
-
-
 // ============ Когда документ загружен ===============
 $(document).ready(function(){
-	let attachmentsCount = fetchAttachmentsCount();
-  clearAttachments();	// это нужно делать ДО получения тела сообщения
+  playAudio('http://127.0.0.1/email-opened.mp3');
+  
+  let attachmentsCount = fetchAttachmentsCount();
+  clearAttachments(); // это нужно делать ДО получения тела сообщения
 
   let msgSubject = fetchSubject();
   let msgBody = fetchBody();
   let msgDate = fetchDate();
-    
-  clearPage();
+  
+  document.title = "Письмо: "+msgSubject;
+  clearPage("письма");
   constructNewPage(msgSubject, msgBody, msgDate, attachmentsCount);
   
-  //$('#hello').focus();
-  //$(".qwerty").first().focus();
+  $('body').append("<textarea style='width:80%; height: 200px;'>Бывший высокопоставленный руководитель Mozilla обвинил Google в умышленном и систематическом саботаже Firefox в течение последнего десятилетия, чтобы ускорить процесс перехода на Chrome. В адрес Google не в первый раз звучат подобные обвинения, однако в первый раз утверждается наличие у Google скоординированного плана, предусматривающего внесение небольших ошибок на своих сайтах, которые будут проявляться только для пользователей Firefox. ");
   
-  playAudio('http://127.0.0.1/email_opened.wav');
 });
