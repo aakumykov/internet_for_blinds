c7b7291e08cc22c4fe0ccabc44519d92824b08a5
Андрей Кумыков <aakumykov@yandex.ru>
2020-05-07 14:40:48 +0700

    clearPage() с параметром: число кнопок-шагов

--- greasemonkey/lib/html_functions.js
+++ greasemonkey/lib/html_functions.js
@@ -8,3 +8,52 @@ function clearPage(stepButtonsLabel) {
 	  $('body').append("<button id='startPoint1'>1 шаг до "+stepButtonsLabel+"</button>");
   }
 }
+
+
+function clearPage(stepButtonsLabel, buttonsCount) {
+	$('body').empty();
+
+	if (null != stepButtonsLabel) 
+	{
+		for (let i=buttonsCount; i>0; i--) 
+		{
+			let id="startPoint"+i;
+			let text = i+" шага";
+			$('body').append("<button id='"+id+"'>"+text+" до "+stepButtonsLabel+"</button>");
+		}
+	}
+}
+
+/*function pluralizeStepWord(number) {
+	let numString=String(n);
+	let latNum = numString.charAt(numString.length - 1);
+
+	switch (latNum) {
+		case 0:
+			return "шагов";
+		case 1:
+			return "шаг";
+		case 2:
+		case 3:
+		case 4:
+			return "шага";
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+			return "шагов";
+	}
+}*/
+
